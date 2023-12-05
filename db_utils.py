1c58c482debd94d62357e3bf21f7bea1cb205e17
c2995509 <alanwatkins159@btinternet.com>
2023-12-05 20:51:26 +0000

    finance project

--- db_utils.py
+++ db_utils.py
@@ -23,33 +23,31 @@ class RDSDatabaseConnector:
         host=connection_details['RDS_HOST']
         port=connection_details['RDS_PORT']
         intro='postgresql+psycopg2://'
-        engine = create_engine(f'{intro}{user}:{password}@{host}:{port}/postgres')
+        engine = create_engine(f'{intro}{user}:{password}@{host}:{port}/{database}')
        # cursor=engine.connect()
        # select_query="Select * From loan_payments"
        # extract_data=cursor.execute(select_query)
-        return engine
-        df=pd.DataFrame(extract_data)
+       ####return engine
+        df=pd.read_sql_table("loan_payments",engine)
         print('connection successful')
-        engine.close()
-        cursor.close()
+       # engine.close()
+       # cursor.close()
 
         return df
       
 
-    def save_csv(self,create_connection):
-        df.to_csv('C:/Users/alanw/AI_CORE_FOLDER/Finance_project.loan_payments.csv', index=False)
+    #def save_csv(self,create_connection):
+       
 
 
-
-        
 
 aws_data=RDSDatabaseConnector()
 
 conn=aws_data.credentials_details() 
-aws_data.create_connection(conn)
+df=aws_data.create_connection(conn)
 
-aws_data=pd.DataFrame()
-csv_df=save_csv()           
-
+#aws_data=pd.DataFrame()
+#sv_df=save_csv()           
+df.to_csv('loan_payments.csv')
 
         
